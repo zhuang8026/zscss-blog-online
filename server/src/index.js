cc865fea15e3ec13b419f70a485c9acbd2edc498
WILLIAM_CHUANG <zhuang8026@gmail.com>
2021-06-06 04:49:41 +0800

    [server]

--- server/src/index.js
+++ server/src/index.js
@@ -149,11 +149,29 @@ io.on("connection", (socket) => {
 });
 
 //*** Start of Routes ***//
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3009");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://zscss.com:*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // server 侦听 3009
 const PORTS = process.env.PORT || 3009; // 符號修改
