25e0ab26742b0c1da3326ec647d99df6b296aff6
WILLIAM_CHUANG <zhuang8026@gmail.com>
2021-06-06 04:56:56 +0800

    [server]

--- server/src/index.js
+++ server/src/index.js
@@ -52,6 +52,31 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+//*** Start of Routes ***//
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 /* ----- socket ----- */
 const server = require("http").createServer(app, (req, res) => {
   // Set CORS headers
@@ -146,31 +171,6 @@ io.on("connection", (socket) => {
     console.log("管理者已登入:", username);
     io.sockets.emit("adminOnline", username);
   });
-});
-
-//*** Start of Routes ***//
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
 });
 
 // server 侦听 3009
